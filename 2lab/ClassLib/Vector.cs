70cfe08b9e7d44087887adc29bb2c0feb33b4538
Stanislay Belovskiy <skweebchik@gmail.com>
2025-10-08 22:54:31 +0300

    lab 2 now done

---
+++ 2lab/ClassLib/Vector.cs
@@ -0,0 +1,52 @@
+﻿namespace ClassLib.Second;
+
+/// <summary>
+/// 3 dimensioned vector
+/// </summary>
+/// <param name="X"></param>
+/// <param name="Y"></param>
+/// <param name="Z"></param>
+public record struct Vector(double X, double Y, double Z)
+{
+    /// <summary>
+    /// vector subtraction first minus second
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
+    public static Vector operator -(Vector first, Vector second) 
+    {
+        var (fx, fy, fz) = first;
+        var (sx, sy, sz) = second;
+
+        return new Vector(fx - sx, fy - sy, fz - sz);
+    }
+
+    /// <summary>
+    /// vector addition 
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
+    public static Vector operator +(Vector first, Vector second) 
+    {
+        var (fx, fy, fz) = first;
+        var (sx, sy, sz) = second;
+
+        return new Vector(fx + sx, fy + sy, fz + sz);
+    }
+
+    /// <summary>
+    /// vector multiplication
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
+    public static Vector operator *(Vector first, Vector second) 
+    {
+        var (fx, fy, fz) = first;
+        var (sx, sy, sz) = second;
+
+        return new Vector(fx * sx, fy * sy, fz * sz);
+    }
+}
---
+++ 2lab/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClassLib.Second;
+
+Console.WriteLine("Введите первый вектор: ");
+var firstVector = EnterVector();
+Console.WriteLine("Введите второй вектор: ");
+var secondVector = EnterVector();
+
+Console.WriteLine($"Векторная разность: { firstVector - secondVector }");
+Console.WriteLine($"Векторная сумма: { firstVector + secondVector }");
+Console.WriteLine($"Векторное произведение: { firstVector * secondVector }");
+
+Vector EnterVector()
+{
+    Console.Write("X: ");
+    double x = double.Parse(Console.ReadLine()!);
+    Console.Write("Y: ");
+    double y = double.Parse(Console.ReadLine()!);
+    Console.Write("Z: ");
+    double z = double.Parse(Console.ReadLine()!);
+    return new Vector(x, y, z);
+}
